24ae5accfc95d8fe76e579ba8434d605c55a0ff2
Reed Renner <reed.renner1@outlook.com>
2025-09-07 18:31:39 -0400

    Update main.tf

--- tutorials/aws-get-started/main.tf
+++ tutorials/aws-get-started/main.tf
@@ -1,34 +1,34 @@
 provider "aws" {
-    region = "us-west-2"
+  region = "us-west-2"
 
-    access_key = "anaccesskey"
-    secret_key = "asecretkey"
-    s3_use_path_style = true
-    skip_credentials_validation = true
-    skip_metadata_api_check = true
-    skip_requesting_account_id = true
+  access_key                  = "anaccesskey"
+  secret_key                  = "asecretkey"
+  s3_use_path_style           = true
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
 
-    endpoints {
-        ec2 = "https://localhost:5466"
-    }
+  endpoints {
+    ec2        = "http://localhost:4566"
+  }
 }
 
 data "aws_ami" "linux" {
-    most_recent = true
+  most_recent = true
 
-    filter {
-        name = "name"
-        values = ["amzn2-ami-hvm-2.0.*-x86_64-ebs"]
-    }
+  filter {
+    name = "name"
+    values = ["amzn2-ami-hvm-2.0.*-x86_64-ebs"]
+  }
 
-    owners = ["137112412989"] # amazon
+  owners = ["137112412989"] # Amazon
 }
 
 resource "aws_instance" "app_server" {
-    ami = data.aws_ami.linux.id
-    instance_type = "t2.micro"
+  ami           = data.aws_ami.linux.id
+  instance_type = "t2.micro"
 
-    tags = {
-        Name = "hashicorp-learn"
-    }
+  tags = {
+    Name = "hashicorp-learn"
+  }
 }
